4b6c8089b3fd704a504af71a2a10d287eeb1e330
Abdul muhamad rosyid <abdulmuhamadrosyid@gmail.com>
2021-02-18 16:03:55 +0700

    add local ip

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 import { Col, Row, Input, Checkbox, Button } from "antd";
+import localIpUrl from "local-ip-url";
 
 import LoginBanner from "../../assets/images/LoginBanner.png";
 import EmailField from "../../components/InputField/EmailField";
@@ -11,6 +12,21 @@ import pathname from "../../pathnameCONFIG";
 
 import "./LoginScreen.css";
 
+let enterprise_id = "istimplenterprise";
+let expires_in =150;
+let message = "You have initiated a transaction. Kindly approve the same";  
+let msg_subject = "Transaction Approval";
+let notification_msg_body = "You have a RELIDverify notification";
+let notification_msg_subject = "RELIDverify notification";
+let base_URL = "https://rel-id.infosyssolusiterpadu.com:8007/DemoVerify/";
+
+// let creds = "ZGVtb3VzZXI6ZGVtb3Bhc3N3b3Jk";
+let creds = "aXN0aW1wbGVudGVycHJpc2U6MjM5ajJx";
+let ds_required = true;
+let notifyCheck =5000;
+let buttons =["Accept","Reject"];
+
+
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -18,31 +34,24 @@ const LoginScreen = () => {
     const [buttonDisable, setButtonDisable] = useState(true);
     const [errorEmail, setErrorEmail] = useState(false);
     const [helperTextEmail, setHelperTextEmail] = useState(null);
+    const [ip, setIp] = useState("");
 
     const handleEmailChange = (value) => {
-        validateEmail(value.target.value);
+        // validateEmail(value.target.value);
         setEmail(value.target.value);
     }
 
-    const validateEmail = value => {
-        const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (!re.test(value) && value.trim().length !== 0) {
-            setErrorEmail(true);
-            setHelperTextEmail("The format of this email is incorrect.");
-        } else {
-            setErrorEmail(false);
-            setHelperTextEmail(null);
-        }
-    }
 
     const handlePasswordChange = (value) => {
         setPassword(value.target.value);
     }
 
     useEffect(() => {
+        setIp(localIpUrl('public', 'ipv4'));
+    }, []);
+
+    useEffect(() => {
         email !== ""
-            && errorEmail !== true
-            && password !== ""
             ? setButtonDisable(false)
             : setButtonDisable(true)
     }, [email, password]);
@@ -62,33 +71,20 @@ const LoginScreen = () => {
                     </div>
                     <div style={{ marginTop: "15px" }}>
                         <div className="input-title">
-                            Email
+                            Username
                         </div>
+                        <br/>
                         <EmailField
                             id={"email"}
                             className="input-field"
-                            placeholder="Enter your email"
+                            placeholder="Enter username"
                             value={email}
                             onChange={handleEmailChange}
                             helperText={helperTextEmail !== null ? helperTextEmail : null}
                             error={errorEmail}
                         />
-                        <div className="input-title" style={{ marginTop: "25px" }}>
-                            Password
-                        </div>
-                        <PasswordField
-                            className="input-field"
-                            value={password}
-                            placeholder="Enter your password"
-                            onChange={handlePasswordChange}
-                        />
                         <Row style={{ marginTop: "17px" }}>
                             <Col span={12}>
-                                <Checkbox
-                                    onChange={(e) => { setRememberUser(e.target.checked) }}
-                                >
-                                    Remember me
-                                </Checkbox>
                             </Col>
                             <Col span={12}>
                                 <div className="forgot-password">
