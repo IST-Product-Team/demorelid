99a2170292ad17d089ee06076ebdb295d872bc40
yoga tomy <>
2021-02-23 11:27:36 +0700

    try fixing cors

--- src/helpers/Axios/interceptor.js
+++ src/helpers/Axios/interceptor.js
@@ -7,9 +7,15 @@ axios.interceptors.request.use(
             // const token = SecureStorage.getItem("credentials").access_token;
             // if (token !== null) {
             //     request.headers.Authorization = `Bearer ${token}`;
-            request.headers.Authorization = `Basic aXN0aW1wbGVudGVycHJpc2U6MjM5ajJx`;
-            request.timeout = 60 * 1000;
-            request.headers.referrerPolicy = `origin-when-cross-origin`;
+            // request.headers.Authorization = `Basic aXN0aW1wbGVudGVycHJpc2U6MjM5ajJx`;
+            // request.timeout = 60 * 1000;
+            // request.headers.ReferrerPolicy = `origin-when-cross-origin`;
+            // request.headers.Access-Control - Allow - Origin: *
+            request.headers = {
+                'Authorization': `Basic aXN0aW1wbGVudGVycHJpc2U6MjM5ajJx`,
+                'ReferrerPolicy': `origin-when-cross-origin`,
+                'Access-Control-Allow-Origin': `*`
+            }
             // }
         } catch (e) {
             return Promise.reject(e);
